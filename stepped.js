7a96765e514bc40c3a74e1f6b28fca053b165143
computerscience41 <rakibhasan934541@gmail.com>
2024-11-10 12:22:55 +0600

    stepped

---
+++ stepped.js
@@ -0,0 +1,13 @@
+const price = 4000;
+
+if(price >= 5000){
+    // 10% discount
+    const discount = price * 10/ 100;
+    const payAmount = price - discount;
+
+    console.log(payAmount);
+}
+
+else{
+    console.log(price);
+}
